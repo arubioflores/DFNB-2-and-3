0ea122caf64dfd085dcbc2380e686d5ba74730d1
arubioflores <55721881+arubioflores@users.noreply.github.com>
2019-12-18 16:56:47 -0700

    Update and rename dbo.v_accounts_per_customer_per_year.sql to Create Views/dbo.v_accounts_per_customer_per_year.sql

---
+++ Create Views/dbo.v_accounts_per_customer_per_year.sql
@@ -0,0 +1,48 @@
+/*****************************************************************************************************************
+NAME:    dbo.v_accounts_per_customer_per_year.sql
+PURPOSE: Create the dbo.v_accounts_per_customer_per_year view
+
+SUPPORT: Alex Rubio 
+         rubioalejandro1114@gmail.com
+         aar43@ldsbc.edu
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+1.0       12/10/2019   ARUBIO      1. Built this view
+1.1       12/10/2019   ARUBIO      2. Added correct formatting
+
+
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(...)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+
+********************************************************************************************/
+DROP VIEW IF EXISTS [dbo].[v_accounts_per_customer_per_year];
+GO
+CREATE VIEW [v_accounts_per_customer_per_year]
+AS
+select DATEPART(YEAR, cd.[cust_since_date]) AS year
+		,cd.[cust_id]
+		,(cd.cust_first_name + ' ' +cd.cust_last_name) as customer_name
+		,cad.cust_role_id
+		,COUNT(cad.[acct_id]) as accounts
+		,cd.[relationship_id]
+		,CONVERT(DECIMAL(10, 0),SUM(ad.loan_amt)) as loans
+from [dbo].[t_customer_dim] as cd
+join [dbo].[t_customer_account_dim] as cad on cd.[cust_id] = cad.cust_id
+join [dbo].[t_account_dim] as ad on ad.acct_id = cad.acct_id
+group by DATEPART(YEAR, cd.[cust_since_date])
+,cd.[cust_id]
+,(cd.cust_first_name + ' ' +cd.cust_last_name)
+,cad.cust_role_id
+,cd.[relationship_id];
