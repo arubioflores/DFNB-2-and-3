12ffad1c17a8bbb6b1e15d1b8a804ff9e0e6c76c
arubioflores <55721881+arubioflores@users.noreply.github.com>
2019-12-18 17:28:42 -0700

    Update Data_Output.sql

--- Data_Output.sql
+++ Data_Output.sql
@@ -1,3 +1,31 @@
+/*****************************************************************************************************************
+NAME:    Data_Output.sql
+PURPOSE: Display data to create reports from
+
+SUPPORT: Alex Rubio 
+         rubioalejandro1114@gmail.com
+         aar43@ldsbc.edu
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+1.0       12/18/2019   ARUBIO      1. Built query to display data
+
+
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(...)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+
+******************************************************************************************************************/
 USE DFNB2
 GO
 
